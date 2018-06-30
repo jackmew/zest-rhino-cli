fc555f8099a0ef00e1d2dd9851af8c5502aa3bc4
ZestHo <zest6789tower@gmail.com>
2018-06-30 16:57:35 +0800

    zest-rhino-singleScreenApp

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const inquirer  = require('./lib/inquirer');
-const start = require('./lib/start');
-var path = require('path');
-var fs = require('fs');
-var execSync = require('child_process').execSync;
 const project = require('./lib/project');
+const files = require('./lib/files');
 
 // clear terminal
 clear();
@@ -17,44 +14,11 @@ console.log(
   )
 );
 
-
 const run = async () => {
-    // 複製檔案
-    // project.create('zestProject4');
-
-    const commands = start.enter();
-    const command = commands[0];
-
-    // switch (command) {
-    //     case 'init':
-    //       if (!commands[1]) {
-    //         console.error('Usage: zest-rhino init <ProjectName> [--verbose]');
-    //         process.exit(1);
-    //       } else {
-    //         start.init(commands[1]);
-    //       }
-    //       break;
-    //     default:
-    //       console.error(
-    //         'Command `%s` unrecognized. ' +
-    //           'Make sure that you have run `npm install` and that you are inside a react-native project.',
-    //         command,
-    //       );
-    //       process.exit(1);
-    //       break;
-    //   }
-    // inquirer.askInitReactNativeProject();
-
-    // const questions = [
-
-    //     {
-    //       type: 'list',
-    //       name: 'visibility',
-    //       message: 'Choose a zest-rhino starter:',
-    //       choices: [ 'zest-rhino-starter', 'zest-rhino-counter' ],
-    //       default: 'zest-rhino-starter'
-    //     }
-    //   ];
-    // return inquirer.prompt(questions);
+    // { name: 'testProject', package: 'com.rhino', 'starter': 'zest-rhino-singleScreenApp' }
+    const answers = await inquirer.askInitReactNativeProject();
+    if(!files.directoryExists(answers.name)) {
+      project.create(answers.name);
+    }
 }
 run();
